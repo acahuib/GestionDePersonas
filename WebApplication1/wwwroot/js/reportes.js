9315fbd75e57811c93daf657cc8970df9c585a16
Anthony <acahuib@unsa.edu.pe>
2026-02-05 13:35:19 -0500

    Se crea el html para especialmente descargar reportes en excel

--- WebApplication1/wwwroot/js/reportes.js
+++ WebApplication1/wwwroot/js/reportes.js
@@ -2,7 +2,7 @@
 // REPORTES HISTÓRICOS
 // ===============================
 
-async function cargarReporte() {
+async function buscarReporte() {
 
     const fecha = document.getElementById("fechaReporte").value;
     const punto = document.getElementById("puntoReporte").value;
@@ -19,17 +19,18 @@ async function cargarReporte() {
     if (tipo) url += `&tipoMovimiento=${tipo}`;
 
     const res = await fetch(url);
-    const data = await res.json();
+    const result = await res.json();
 
-    renderTablaReportes(data);
+    renderTablaReporte(result.data);
 }
 
-function renderTablaReportes(datos) {
 
-    const tbody = document.getElementById("tablaReportes");
+function renderTablaReporte(data) {
+
+    const tbody = document.querySelector("#tablaReporte tbody");
     tbody.innerHTML = "";
 
-    if (datos.length === 0) {
+    if (data.length === 0) {
         tbody.innerHTML = `
             <tr>
                 <td colspan="5">No hay registros para los filtros seleccionados</td>
@@ -38,7 +39,7 @@ function renderTablaReportes(datos) {
         return;
     }
 
-    datos.forEach(r => {
+    data.forEach(r => {
         const fecha = new Date(r.fechaHora).toLocaleString();
 
         tbody.innerHTML += `
@@ -51,4 +52,23 @@ function renderTablaReportes(datos) {
             </tr>
         `;
     });
+}
+function exportarExcel() {
+
+    const fecha = document.getElementById("fechaReporte").value;
+    const punto = document.getElementById("puntoReporte").value;
+    const tipo = document.getElementById("tipoReporte").value;
+
+    if (!fecha) {
+        alert("Seleccione una fecha antes de exportar");
+        return;
+    }
+
+    let url = `${API_BASE}/reportes/export/excel?fecha=${fecha}`;
+
+    if (punto) url += `&puntoControlId=${punto}`;
+    if (tipo) url += `&tipoMovimiento=${tipo}`;
+
+    // Abrir descarga directa
+    window.open(url, "_blank");
 }
